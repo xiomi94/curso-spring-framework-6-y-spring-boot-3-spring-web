f9b773ba59630f0dbf3c5d3953f70c9cc1dcee3e
xiomi94 <xiomarajimvel94@gmail.com>
2025-05-14 08:38:53 +0100

    15. Anotación @RequestMapping

--- src/main/java/com/xiomara/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/xiomara/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,16 +1,20 @@
 package com.xiomara.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
-
-  @GetMapping("/details2")
+  
+// @RequestMappi8ng(path="/details", method=RequestMethod.GET) /Esta es otra manera de hacerlo
+  @GetMapping("/details")
   public Map<String, Object> details() {
     Map<String, Object> body = new HashMap<>();
+
     body.put("title", "Hola Mundo Spring Boot");
     body.put("name", "Xiomara");
     body.put("lastname", "Jiménez");
