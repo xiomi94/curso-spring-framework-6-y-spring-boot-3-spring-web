1e4853a37a3c8de659f73bbc7cb141ec83c52430
xiomi94 <xiomarajimvel94@gmail.com>
2025-05-14 09:47:09 +0100

    16. El objeto Model

--- src/main/java/com/xiomara/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/xiomara/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,9 +1,13 @@
 package com.xiomara.curso.springboot.webapp.springbootweb.controllers;
 
+import com.xiomara.curso.springboot.webapp.springbootweb.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,11 +17,11 @@ public class UserRestController {
 // @RequestMappi8ng(path="/details", method=RequestMethod.GET) /Esta es otra manera de hacerlo
   @GetMapping("/details")
   public Map<String, Object> details() {
+    User user = new User("Xiomara", "Jiménez", "xomi94@gmail.com", "54136071M ");
     Map<String, Object> body = new HashMap<>();
 
     body.put("title", "Hola Mundo Spring Boot");
-    body.put("name", "Xiomara");
-    body.put("lastname", "Jiménez");
+    body.put("user", user);
     return body;
   }
 }
---
+++ src/main/java/com/xiomara/curso/springboot/webapp/springbootweb/models/User.java
@@ -0,0 +1,49 @@
+package com.xiomara.curso.springboot.webapp.springbootweb.models;
+
+public class User {
+
+  private String name;
+  private String lastName;
+  private String email;
+  private String dni;
+
+
+  public User(String name, String lastName, String email, String dni) {
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.dni = dni;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getDni() {
+    return this.dni;
+  }
+
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
+}
